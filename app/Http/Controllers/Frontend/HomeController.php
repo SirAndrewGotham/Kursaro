c8be2421e442689d3988126e3b91dfe996ad2db2
AndrewGotham <andrewgotham@hotmail.com>
2024-12-05 09:17:48 +0300

    enabled home and page view count

--- app/Http/Controllers/Frontend/HomeController.php
+++ app/Http/Controllers/Frontend/HomeController.php
@@ -12,26 +12,13 @@ class HomeController
 {
     public function __invoke()
     {
-//        $user = auth()->user();
-//
-//        $roles            = Role::with('permissions')->get();
-//        $permissionsArray = [];
-//
-//        foreach ($roles as $role) {
-//            foreach ($role->permissions as $permissions) {
-//                $permissionsArray[$permissions->title][] = $role->id;
-//            }
-//        }
-//
-//        foreach ($permissionsArray as $title => $roles) {
-//            Gate::define($title, fn(User $user) => $user->is_admin);
-////            Gate::define($title, function ($user) use ($roles) {
-////                return count(array_intersect($user->roles->pluck('id')->toArray(), $roles)) > 0;
-////            });
-//        }
-
         $lang = Language::where('code', app()->getLocale())->first()->id;
         $model = Home::where([['is_active', true],['language_id', $lang]])->first() ?? Home::where('is_default', true)->first();
+
+        $model->timestamps = false;
+        $model->views++;
+        $model->save();
+
         $content = $model->content;
 
         return view('frontend.default.home.index', compact('content'));
